037082d6d50eb3a711e183d8ca3b4913fc1ca792
jishant07 <jishanta@gmail.com>
2024-10-04 18:30:59 +0530

    Added Init Code With Dockerfile

---
+++ src/test/java/com/mossad/assignment/HelloWorldApplication/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mossad.assignment.HelloWorldApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
